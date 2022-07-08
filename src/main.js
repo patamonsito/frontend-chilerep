f300d17d9d243d5dab4c999dff5c45ca1b5d3d0a
luis sanchez <patamonsitoyt@gmail.com>
2022-07-08 10:04:30 -0400

    ñe

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import AppAdmin from './App-admin-pc.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-import axios from 'axios';
-
-axios.defaults.withCredentials = true;
 
 Vue.config.productionTip = false
 
